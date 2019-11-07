b3dbcb3dc406772941a405cb843248a61ca60b7b
Paritosh Mishra <paritosh149@outlook.com>
2019-11-08 10:10:44 +1100

    Link Text changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to ReactJS.org
         </a>
       </header>
     </div>
